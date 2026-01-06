ad2ff8ca1d6f7bc44fac102e2d4aad8f59c5a4b5
Caleb Reagor <ccreagor@gmail.com>
2026-01-06 15:39:15 +0100

    GRNs for all placodes (E8/9)

--- run-pyscenic.sh
+++ run-pyscenic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NUM_WORKERS=8
+NUM_WORKERS=16
 RANK_THRESH=1000
 AUC_THRESH=0.01
 TOP_N=20
@@ -9,37 +9,38 @@ MIN_GENES=10
 
 PTH_DATA="data/scenic"
 PTH_REF="data/cisTarget"
-FN_LOOM="${PTH_DATA}/e8_placodes.loom"
-FN_GRN="${PTH_DATA}/grn.tsv"
-FN_REG="${PTH_DATA}/regulons.gmt"
 
-# redirect output to logfile
-TIME=$(date +"%Y%m%d_%H%M%S")
-FN_LOG="${PTH_DATA}/${TIME}.log"
-exec > >(tee "${FN_LOG}") 2>&1
+for DAY in "E8" "E9"; do
 
-# GRN inference
-pyscenic grn \
-    -o "${FN_GRN}" \
-    -m grnboost2 \
-    --seed 1 \
-    --num_workers "${NUM_WORKERS}" \
-    --cell_id_attribute obs_names \
-    --gene_attribute var_names \
-    "${FN_LOOM}" \
-    "${PTH_REF}"/allTFs_mm.txt
+    PTH_DAY="${PTH_DATA}/${DAY}"
+    FN_LOOM="${PTH_DAY}/placodes.loom"
+    FN_GRN="${PTH_DAY}/grn.tsv"
+    FN_REG="${PTH_DAY}/regulons.gmt"
 
-# prune edges
-pyscenic ctx \
-    -o "${FN_REG}" \
-    --rank_threshold "${RANK_THRESH}" \
-    --auc_threshold "${AUC_THRESH}" \
-    --annotations_fname "${PTH_REF}/"*.tbl \
-    --num_workers "${NUM_WORKERS}" \
-    --top_n_regulators "${TOP_N}" \
-    --min_genes "${MIN_GENES}" \
-    --expression_mtx_fname "${FN_LOOM}" \
-    --cell_id_attribute obs_names \
-    --gene_attribute var_names \
-    "${FN_GRN}" \
-    "${PTH_REF}/"*.feather
+    # GRN inference
+    pyscenic grn \
+        -o "${FN_GRN}" \
+        -m grnboost2 \
+        --seed 1 \
+        --num_workers "${NUM_WORKERS}" \
+        --cell_id_attribute obs_names \
+        --gene_attribute var_names \
+        "${FN_LOOM}" \
+        "${PTH_REF}"/allTFs_mm.txt
+
+    # prune edges
+    pyscenic ctx \
+        -o "${FN_REG}" \
+        --rank_threshold "${RANK_THRESH}" \
+        --auc_threshold "${AUC_THRESH}" \
+        --annotations_fname "${PTH_REF}/"*.tbl \
+        --num_workers "${NUM_WORKERS}" \
+        --top_n_regulators "${TOP_N}" \
+        --min_genes "${MIN_GENES}" \
+        --expression_mtx_fname "${FN_LOOM}" \
+        --cell_id_attribute obs_names \
+        --gene_attribute var_names \
+        "${FN_GRN}" \
+        "${PTH_REF}/"*.feather
+
+done
